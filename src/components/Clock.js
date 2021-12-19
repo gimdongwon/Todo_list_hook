8eb75b70220cfe6d426241c5770e2c076de59b3f
gimdongwon <ehddnjs8989@gmail.com>
2021-12-19 19:31:40 +0900

    fixed import error

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
